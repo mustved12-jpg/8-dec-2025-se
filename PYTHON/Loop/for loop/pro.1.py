b523e92f8fcb73a3bcffdb5f79da2a79095ef69e
mustved.jpg <mustved12>
2026-03-31 20:11:38 +0530

    add

---
+++ PYTHON/Loop/for loop/pro.1.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print(i,)
+
+print()# this is for new line
+
+for i in range(1,6):# hear 1 is start point and 6 is end point loop will run total 5 time
+    print(i,)
+
+print()
+
+for i in range(5):
+    print("hello",end=" ")# using of end it will print output in single row like hello hello.....
+
+print("\n")
+
+for i in range(1,7):
+    print(f"({i}).hello",end=" ")
+
+print("\n")
+
+for i in range(5,0,-1):
+    print(f"({i}).HELLO",)
